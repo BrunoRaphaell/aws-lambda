76854d914896a99944c1929f6b7f8effe5a68a3e
BrunoRaphaell <brunoraphaell99@gmail.com>
2023-06-15 15:59:01 -0300

    teste extensão vscode

--- log.py
+++ log.py
@@ -1,2 +1,2 @@
 def mensagem_log(mensagem):
-    return 'Mensagem após ter enviado o código para o git actions: ' + mensagem
+    return 'Mensagem após ter enviado o código para o git actions, testando com a extensão do vscode: ' + mensagem
